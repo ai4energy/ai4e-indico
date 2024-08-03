d0cb5fb34c03a5acf357c8be177d9d2cb895ca40
mingtaoli <mingtao@xjtu.edu.cn>
2024-08-03 21:16:09 +0800

    修改环境变量和心跳脚本

--- dockerfiles/indico-worker/run_celery.sh
+++ dockerfiles/indico-worker/run_celery.sh
@@ -3,10 +3,12 @@
 /opt/indico/set_user.sh
 . /opt/indico/.venv/bin/activate
 
-echo 'Waiting for indico-web to be online...'
-while [[ "$(curl -L --max-time 10 -s -o /dev/null -w ''%{http_code}'' 'http://indico-web:59999')" != "200" ]]; do
-    sleep 10;
-    echo 'Waiting...'
+SERVICE_URL="${SERVICE_PROTOCOL}://${SERVICE_HOSTNAME}:${SERVICE_PORT:-80}"
+
+echo "Waiting for $SERVICE_URL to be online..."
+while [[ "$(curl -L --max-time 10 -s -o /dev/null -w '%{http_code}' "$SERVICE_URL")" != "200" ]]; do
+    sleep 10
+    echo "Waiting..."
 done
 
 echo 'Starting Celery...'
